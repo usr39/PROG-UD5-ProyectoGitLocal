19b2713b6e95228b4cd5d5eb053b1509731a180d
usr39 <usr3919@salesianos-lacuesta.net>
2025-04-29 10:12:48 +0100

    feat(App.java):Welcome message

--- src/net/salesianos/humanresources/Person.java
+++ src/net/salesianos/humanresources/Person.java
@@ -9,4 +9,22 @@ public class Person {
         this.age = age;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+
 }
