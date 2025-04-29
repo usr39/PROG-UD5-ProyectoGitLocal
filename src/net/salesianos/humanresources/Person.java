b862050852543e5b039219e5b1279af640f99b1f
usr39 <usr3919@salesianos-lacuesta.net>
2025-04-29 09:58:10 +0100

    feat(Person.java): added all properties

---
+++ src/net/salesianos/humanresources/Person.java
@@ -0,0 +1,12 @@
+package net.salesianos.humanresources;
+
+public class Person {
+    private String nombre;
+    private int age;
+
+    public Person(String nombre, int age) {
+        this.nombre = nombre;
+        this.age = age;
+    }
+
+}
