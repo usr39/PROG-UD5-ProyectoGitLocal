3f3565f28541f50c60128c6be5bd4dd0fd413a39
usr39 <usr3919@salesianos-lacuesta.net>
2025-04-29 10:20:59 +0100

    feat(App.java): Display menu options

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Menú:");
+        System.out.println("-OPción 1: añadir persona");
+        System.out.println("- OPCIÓN 2: borrar persona");
+        System.out.println("- OPCIÓN 3: salir");
     }
 }
